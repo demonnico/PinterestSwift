eb8aee51b1e587c25ef036ed1ba25ca6fe73f7f1
Nicholas Tau <fengping.tao@upai.com>
2014-07-01 14:38:17 +0800

    demo improvement

--- PinterestSwift/ViewController.swift
+++ PinterestSwift/ViewController.swift
@@ -8,32 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController,UICollectionViewDataSource, UICollectionViewDelegate{
+class ViewController: UIViewController,UICollectionViewDataSource, UICollectionViewDelegate, CHTCollectionViewDelegateWaterfallLayout{
                             
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.yellowColor()
         
         var layout: CHTCollectionViewWaterfallLayout = CHTCollectionViewWaterfallLayout()
+        
         var collectionView = UICollectionView(frame: CGRectMake(0, 0, 320, 480), collectionViewLayout: layout)
         view.addSubview(collectionView)
         
+        collectionView.backgroundColor = UIColor.grayColor()
         collectionView.delegate = self
         collectionView.dataSource = self
-        collectionView.registerClass(NTWaterfallViewCell.superclass(), forCellWithReuseIdentifier: "identify")
+        collectionView.registerClass(NTWaterfallViewCell.self, forCellWithReuseIdentifier: "identify")
+        collectionView.reloadData()
     }
     
-    func collectionView(collectionView: UICollectionView!, cellForItemAtIndexPath indexPath: NSIndexPath!) -> UICollectionViewCell!
-    {
+    func collectionView(collectionView: UICollectionView!, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize{
+        return CGSizeMake(145, 120)
+    }
+    
+    func collectionView(collectionView: UICollectionView!, cellForItemAtIndexPath indexPath: NSIndexPath!) -> UICollectionViewCell!{
         var collectionCell: NTWaterfallViewCell = collectionView.dequeueReusableCellWithReuseIdentifier("identify", forIndexPath: indexPath) as NTWaterfallViewCell
         return collectionCell;
     }
     
-    func collectionView(collectionView: UICollectionView!, numberOfItemsInSection section: Int) -> Int
-    {
-        return 1;
+    func collectionView(collectionView: UICollectionView!, numberOfItemsInSection section: Int) -> Int{
+        return 100;
     }
-
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView!) -> Int{
+        return 1
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
