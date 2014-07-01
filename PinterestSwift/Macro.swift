55a8a27021852b84ef6b0a936682a904f5430f5c
Nicholas Tau <fengping.tao@upai.com>
2014-07-01 18:12:36 +0800

    add file

---
+++ PinterestSwift/Macro.swift
@@ -0,0 +1,15 @@
+//
+//  Macro.swift
+//  PinterestSwift
+//
+//  Created by Nicholas Tau on 7/1/14.
+//  Copyright (c) 2014 Nicholas Tau. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let screenBounds = UIScreen.mainScreen().bounds
+let screenSize   = screenBounds.size
+let screenWidth  = screenSize.width
+let screenHeight = screenSize.height
