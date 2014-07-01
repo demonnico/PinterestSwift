1046c4f54fa07935b86ba1cbb9e51887ed054092
Nicholas Tau <fengping.tao@upai.com>
2014-07-01 16:04:41 +0800

    add files

---
+++ PinterestSwift/NTHorizontalPageViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  NTHorizontalPageViewCell.swift
+//  PinterestSwift
+//
+//  Created by Nicholas Tau on 7/1/14.
+//  Copyright (c) 2014 Nicholas Tau. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NTHorizontalPageViewCell : UICollectionViewCell{
+    init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+}
---
+++ PinterestSwift/NTHorizontalPageViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NTHorizontalPageViewController.swift
+//  PinterestSwift
+//
+//  Created by Nicholas Tau on 7/1/14.
+//  Copyright (c) 2014 Nicholas Tau. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NTHorizontalPageViewController : UICollectionViewController{
+ 
+    override func viewDidLoad(){
+        super.viewDidLoad()
+    }
+    
+    
+}
