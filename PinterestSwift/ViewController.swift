3975f79bfe2f2fbb54e70679ffa44e34babb837a
Nicholas Tau <fengping.tao@upai.com>
2014-07-01 12:10:35 +0800

    add UICollectionView into UIViewController

--- PinterestSwift/ViewController.swift
+++ PinterestSwift/ViewController.swift
@@ -8,11 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UICollectionViewDataSource, UICollectionViewDelegate{
                             
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var layout: CHTCollectionViewWaterfallLayout = CHTCollectionViewWaterfallLayout()
+        var collectionView = UICollectionView(frame: CGRectMake(0, 0, 320, 480), collectionViewLayout: layout)
+        view.addSubview(collectionView)
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.registerClass(NTWaterfallViewCell.superclass(), forCellWithReuseIdentifier: "identify")
+    }
+    
+    func collectionView(collectionView: UICollectionView!, cellForItemAtIndexPath indexPath: NSIndexPath!) -> UICollectionViewCell!
+    {
+        var collectionCell: NTWaterfallViewCell = collectionView.dequeueReusableCellWithReuseIdentifier("identify", forIndexPath: indexPath) as NTWaterfallViewCell
+        return collectionCell;
+    }
+    
+    func collectionView(collectionView: UICollectionView!, numberOfItemsInSection section: Int) -> Int
+    {
+        return 1;
     }
 
     override func didReceiveMemoryWarning() {
