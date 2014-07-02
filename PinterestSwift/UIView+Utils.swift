f1c06950a443d6c86588b160eb194f3d5c2d5811
Nicholas Tau <fengping.tao@upai.com>
2014-07-02 12:12:22 +0800

    add extension

---
+++ PinterestSwift/UIView+Utils.swift
@@ -0,0 +1,21 @@
+//
+//  UIView+Utils.swift
+//  PinterestSwift
+//
+//  Created by Nicholas Tau on 7/2/14.
+//  Copyright (c) 2014 Nicholas Tau. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIView{
+    var origin : CGPoint{
+    set{
+//        frame.origin.x = newValue.x
+//        frame.origin.y = newValue.y
+    }
+    get{
+        return CGPointZero//frame.origin
+    }}
+}
