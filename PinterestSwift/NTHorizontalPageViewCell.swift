ec3edeb962afc76f2c0fc7941538b41a21b7d6a8
Nicholas Tau <fengping.tao@upai.com>
2014-07-02 10:25:59 +0800

    custom tableview

--- PinterestSwift/NTHorizontalPageViewCell.swift
+++ PinterestSwift/NTHorizontalPageViewCell.swift
@@ -9,20 +9,70 @@
 import Foundation
 import UIKit
 
-class NTHorizontalPageViewCell : UICollectionViewCell{
-    var imageName : String = " "
-    var imageViewContent : UIImageView = UIImageView()
-    init(frame: CGRect) {
-        super.init(frame: frame)
-        backgroundColor = UIColor.lightGrayColor()
-        contentView.addSubview(imageViewContent)
+let cellIdentify = "cellIdentify"
+
+class NTTableViewCell : UITableViewCell{
+    
+    init(style: UITableViewCellStyle, reuseIdentifier: String!) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.textLabel.font = UIFont.systemFontOfSize(13)
     }
     
     override func layoutSubviews() {
-        let image = UIImage(named: imageName)
-        let imageHeight = image.size.height*screenWidth/image.size.width
-        imageViewContent.frame = CGRectMake(0, 0, screenWidth, imageHeight)
-        imageViewContent.image = UIImage(named: imageName)
         super.layoutSubviews()
+        imageView.frame = CGRectZero
+        if imageView.image {
+            let imageHeight = imageView.image.size.height*screenWidth/imageView.image.size.width
+            imageView.frame = CGRectMake(0, 0, screenWidth, imageHeight)
+        }
+    }
+}
+
+class NTHorizontalPageViewCell : UICollectionViewCell, UITableViewDelegate, UITableViewDataSource{
+    var imageName : String = " "
+    let tableView = UITableView(frame: screenBounds, style: UITableViewStyle.Plain)
+    init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = UIColor.lightGrayColor()
+        
+        
+        contentView.addSubview(tableView)
+        tableView.registerClass(NTTableViewCell.self, forCellReuseIdentifier: cellIdentify)
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        tableView.reloadData()
+    }
+    
+    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(tableView: UITableView!, heightForRowAtIndexPath indexPath: NSIndexPath!) -> CGFloat{
+        var cellHeight : CGFloat = 44
+        if indexPath.row == 0{
+            let image = UIImage(named: imageName)
+            let imageHeight = image.size.height*screenWidth/image.size.width
+            cellHeight = imageHeight
+        }
+        return cellHeight
+    }
+    
+    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell!
+    {
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentify) as NTTableViewCell!
+        cell.imageView.image = nil
+        cell.textLabel.text = nil
+        if indexPath.row == 0 {
+            let image = UIImage(named: imageName)
+            cell.imageView.image = image
+        }else{
+            cell.textLabel.text = "you'd custom this table view as you will"
+        }
+        cell.setNeedsLayout()
+        return cell
     }
 }
