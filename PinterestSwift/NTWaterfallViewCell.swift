de09f476ce244c137f5a6aa3b8e36f26d71e7642
Nicholas Tau <fengping.tao@upai.com>
2014-08-25 19:00:11 +0800

    It's better to by this way.

--- PinterestSwift/NTWaterfallViewCell.swift
+++ PinterestSwift/NTWaterfallViewCell.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class NTWaterfallViewCell :UICollectionViewCell, NTTansitionWaterfallGridViewProtocol{
-    var imageName : String = " "
+    var imageName : String?
     var imageViewContent : UIImageView = UIImageView()
     required init(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
